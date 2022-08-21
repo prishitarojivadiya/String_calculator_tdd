92a3ab14a60ad37513869bb0356a6b87996ebcbc
Prishita Rojivadiya <prishita.rojivadiya1@gmail.com>
2022-08-22 01:34:49 +0530

    Testcase succeed  : test_multiple_coma_seperated_string (addition of multiple coma seperated string)

--- String_calculator.py
+++ String_calculator.py
@@ -6,4 +6,10 @@ class String_calculator:
             return 0
         elif len(strings) == 1:
             return int(strings)
-        
+        else:
+            list_of_comaseperated_string = strings.split(",")
+            sum=0
+            for i in list_of_comaseperated_string:
+                i = int(i)
+                sum = sum+i
+            return sum
--- Test_string_calculator.py
+++ Test_string_calculator.py
@@ -1,3 +1,4 @@
+import string
 import unittest
 from unittest import result
 from String_calculator import String_calculator
@@ -8,5 +9,8 @@ class Test_string_calculator(unittest.TestCase):
     def test_one_string(self):
         result=String_calculator.add("1")
         self.assertEqual(result,1)
+    def test_multiple_coma_seperated_string(self):
+        result=String_calculator.add("1,2,3")
+        self.assertEqual(result,6)
     
         
